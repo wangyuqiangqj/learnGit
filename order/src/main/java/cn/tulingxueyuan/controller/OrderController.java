d756fc0054278f351d8654ebb43300f8df234389
wangyuqiang <wangyuqiangqj@163.com>
2025-02-16 23:14:51 +0800

    第二次提交，优化下单接口

--- order/src/main/java/cn/tulingxueyuan/controller/OrderController.java
+++ order/src/main/java/cn/tulingxueyuan/controller/OrderController.java
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/order")
@@ -13,12 +17,23 @@ public class OrderController {
     @Value("${server.port}")
     private int port;
 
+    private String stockServiceUrl = "http://localhost:8010";
+
     @Autowired
     private RestTemplate restTemplate;
 
     @RequestMapping("/add")
-    public String addOrder(){
-        String result = restTemplate.getForObject("http://localhost:8010/stock/reduce", String.class);
-        return "下单成功" + port + "--" + result;
+    public String addOrder(Long goodsId){
+        try {
+            URI uri = UriComponentsBuilder.fromHttpUrl(stockServiceUrl)
+                    .path("/stock/reduce")
+                    .queryParam("goodsId", goodsId)
+                    .build()
+                    .toUri();
+            String result = restTemplate.getForObject(uri, String.class);
+            return "下单成功" + port + "--" + result;
+        } catch (RestClientException e) {
+            return "下单失败，库存服务不可用";
+        }
     }
 }
